0a34f6287a0254187cdb4c9a8905416e3ac3a6ce
RBITS1 <rgb907@hotmail.com>
2017-02-26 10:41:04 -0500

    initialize AbovetheTreeline
    initialize AbovetheTreeline

---
+++ AbovetheTreeline/AbovetheTreeline/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AbovetheTreeline.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AbovetheTreeline/AbovetheTreeline/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AbovetheTreeline.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
