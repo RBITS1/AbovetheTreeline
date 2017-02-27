3cc00b215924f1a34aed99ed359a6811e9ea6656
RBITS1 <rgb907@hotmail.com>
2017-02-26 23:13:59 -0500

    data layer class
    data layer class

---
+++ AbovetheTreeline/AbovetheTreeline.data/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbovetheTreeline.data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public BookDetails bookdetail { get; set; }
+    }
+}
---
+++ AbovetheTreeline/AbovetheTreeline.data/BookDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbovetheTreeline.data
+{
+    public class BookDetails
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+    }
+}
